a1f9a8f4729d98ac9c1ca3cbf223a9016e9360b6
Lionello <dragonheart.lion@hotmail.it>
2015-04-22 19:55:25 +0200

    Prova

---
+++ Prova/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int x=1;
+		System.out.format("x=%d%n", x);
+		System.out.println("ok");
+	}
+}
---
+++ Prova/src/fin.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+
+public class fin extends JFrame {
+	
+	ImageIcon n=new ImageIcon("Imm.jpg");
+	JButton b=new JButton();
+	
+	public fin(){
+		this.add(b);
+		b.setIcon(n);
+		this.setVisible(true);
+		this.setSize(500,500);
+	}
+}
